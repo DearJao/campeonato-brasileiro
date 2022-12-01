24efc497a74ee4794b45ccd8eee70ef88e83194d
DearJao <jvvr2013@gmail.com>
2022-12-01 16:35:38 -0300

    teste 4

--- app/backend/src/service/MatchesService.ts
+++ app/backend/src/service/MatchesService.ts
@@ -14,6 +14,20 @@ class MatchesService {
 
     return matchesList;
   }
+
+  // static async getInPrgressMatches(inProgress: boolean): Promise<IMatches[]> {
+  //   const matchesList: IMatches[] = await MatchesModel.findAll({
+  //     where: { inProgress },
+  //     include: {
+  //       all: true,
+  //       attributes: {
+  //         exclude: ['id'],
+  //       },
+  //     },
+  //   });
+
+  //   return matchesList;
+  // }
 }
 
 export default MatchesService;
